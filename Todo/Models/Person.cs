cb72415a6e3e0f78b9a1cffd2305ad28dfc9d813
Uzza <salamsyr12213@gmail.com>
2022-09-30 02:26:44 +0200

    Add project files.

---
+++ Todo/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public class Person
+    {
+        public string id { get; }
+        public string firstname { get; }
+        public string secondname { get; }
+        {
+
+        }
+    }
+}
